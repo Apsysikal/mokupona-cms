0ef45bdfdb41cc211a73424a73b86d081aa08b04
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2023-06-23 17:38:05 +0200

    Adding automatic admin creation in development

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-"use strict";
-
 module.exports = {
   /**
    * An asynchronous register function that runs before
@@ -16,5 +14,34 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap({ strapi }: { strapi: Strapi.Strapi }) {
+    if (process.env.NODE_ENV === "development") {
+      const adminUserExists = await strapi.service("admin::user").exists();
+
+      if (adminUserExists) {
+        console.info("Admin user already exists. Skipping admin user creation");
+        return;
+      }
+
+      const adminRole = await strapi.service("admin::role").getSuperAdmin();
+
+      if (!adminRole) {
+        console.info(
+          "Super Admin role doesn't exist. Skipping admin user creation"
+        );
+        return;
+      }
+
+      await strapi.service("admin::user").create({
+        username: "Admin",
+        password: "lets-dev",
+        firstname: "John",
+        lastname: "Doe",
+        email: "dev@mokupona.ch",
+        blocked: false,
+        isActive: true,
+        roles: [adminRole.id],
+      });
+    }
+  },
 };
